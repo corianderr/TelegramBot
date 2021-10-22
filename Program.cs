9bae9e398aa0006be4d5a54ad0c508879f54a47c
Karina Andriushchenko <karina.andr@icloud.com>
2021-10-22 14:59:03 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace cw69
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bot bot = new Bot("2086799401:AAGB5TRQtKSRrCS1lNKhXSV8EefvUH0gjMc");
+            bot.StartBot();
+        }
+    }
+}
