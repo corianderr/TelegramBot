3e3aab2be19658b7e84b5bda1b1da8de3c4ecf2d
Karina Andriushchenko <karina.andr@icloud.com>
2021-10-22 17:54:06 +0600

    #1 Added actions on '/get', '/help', '/game' options. Added game functional.

--- Bot.cs
+++ Bot.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Args;
 using Telegram.Bot.Types;
@@ -33,19 +34,70 @@ namespace cw69
             {
                 Message message = messageEventArgs.Message;
                 Console.WriteLine(message.Text);
-                var markup = new InlineKeyboardMarkup(new[]
+                if (message.Text == "/game")
                 {
-                   new InlineKeyboardButton(){Text = "Время", CallbackData = DateTime.Now.ToShortTimeString()},
-                   new InlineKeyboardButton(){Text = "Приветствие", CallbackData = "Здравствуйте"},
-                   new InlineKeyboardButton(){Text = "Hello", CallbackData = "Hello"}
-                });
-                await _bot.SendTextMessageAsync(message.Chat.Id, message.Text, replyMarkup: markup);
+                    var botsChoice = RandomChoice();
+                    var markup = new InlineKeyboardMarkup(new[]
+                    {
+                       new InlineKeyboardButton(){Text = "Rock", CallbackData = RockPaperScissors(botsChoice, "rock"), },
+                       new InlineKeyboardButton(){Text = "Paper", CallbackData = RockPaperScissors(botsChoice, "paper")},
+                       new InlineKeyboardButton(){Text = "Scissors", CallbackData = RockPaperScissors(botsChoice, "scissors")}
+                    });
+                    await _bot.SendTextMessageAsync(message.Chat.Id, "Make your choice!", replyMarkup: markup);
+                }
+                else if (message.Text == "/start")
+                {
+                    var markup = new ReplyKeyboardMarkup(new[]
+                    {
+                       new KeyboardButton("/help"),
+                       new KeyboardButton("/game"),
+                    });
+                    await _bot.SendTextMessageAsync(message.Chat.Id, "Вас приветствует бот 69 домашки! Введите/выберите команду /help, чтобы ознакомитья с правилами игры; или команду /game, чтобы начать игру.", replyMarkup: markup);
+                }
+                else if (message.Text == "/help")
+                {
+                    var markup = new ReplyKeyboardMarkup(new[]
+                    {
+                       new KeyboardButton("/game"),
+                    });
+                    await _bot.SendTextMessageAsync(message.Chat.Id, "После вызова команды /game бот загадает " +
+                        "в случайном порядке одну из трех позиций, камень, ножницы или бумага, в свою очередь вы загадываете свою позицию, " +
+                        "выбирая одну из кнопок. После вашего хода, бот выведет сообщение о том что он загадал, а также кто из вас победил. нажмите /game, чтобы начать игру.", replyMarkup: markup);
+                }
+                else
+                {
+                    await _bot.SendTextMessageAsync(message.Chat.Id, "You should enter /game to start the game");
+                }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
             }
         }
+        public static string RandomChoice()
+        {
+            Random r = new Random();
+            int condition = r.Next(3);
+            string result = condition switch
+            {
+                0 => "rock",
+                1 => "paper",
+                2 => "scissors",
+                _ => throw new Exception("Oh ooh")
+            };
+            return result;
+        }
+        public static string RockPaperScissors(string first, string second)
+        => (first, second) switch
+        {
+            ("rock", "paper") => "rock is covered by paper. Your paper wins.",
+            ("rock", "scissors") => "rock breaks scissors. My rock wins.",
+            ("paper", "rock") => "paper covers rock. My paper wins.",
+            ("paper", "scissors") => "paper is cut by scissors. Your scissors wins.",
+            ("scissors", "rock") => "scissors is broken by rock. Your rock wins.",
+            ("scissors", "paper") => "scissors cuts paper. My scissors wins.",
+            (_, _) => "tie"
+        };
         private async void HandleCallbackQuery(object sender, CallbackQueryEventArgs callbackQueryEventArgs)
         {
             await _bot.AnswerCallbackQueryAsync(callbackQueryEventArgs.CallbackQuery.Id,
